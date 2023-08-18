ec440af69885ec2bda01d3a9452d3f3dabc2cf16
hisahito <hisahito706@gmail.com>
2023-08-18 13:21:43 -0700

    /user /multilateral and /getHash FEA endpoint OK

--- src/Api.js
+++ src/Api.js
@@ -2,62 +2,172 @@ import axios from 'axios';
 
 const BASE_URL = 'https://feb.seguridata.com/ssignrest';
 
-
+/**
+ * Autentica al usuario y devuelve un token.
+ * @param {string} user - Nombre de usuario.
+ * @param {string} password - Contraseña del usuario.
+ * @returns {string} Token de autenticación.
+ * @throws {Error} Si la autenticación falla o hay un error en la solicitud.
+ */
 export async function authenticate(user, password) {
     try {
-      const response = await axios.post(`${BASE_URL}/user`, null, {
-        params: {
-          user: user,
-          password: password
+        const response = await axios.post(`${BASE_URL}/user`, null, {
+            params: {
+                user: user,
+                password: password
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data.token;
+        } else {
+            throw new Error(response.data.description || 'Error desconocido durante la autenticación.');
         }
-      });
-  
-      if (response.status === 200) {
-        return response.data.token;
-      } else {
-        throw new Error(response.data.description);
-      }
-    } catch (error) {
-      throw new Error(error.response.data.description);
-    }
-  }  
-
-  export function convertFileToBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result.split(',')[1]);
-      reader.onerror = error => reject(error);
-    });
-  }
-
- export async function startMultilateralProcess(token, data, document2Sign, hashAlg, processType, totalSigners) {
-    const headers = {
-        'Authorization': token,
-      };      
-  
-    try {
-      const response = await axios.post(`${BASE_URL}/multilateral/`, {
-        data: data,
-        document2Sign: {
-          base64: true,
-          data: document2Sign, // Convierte el archivo a Base64
-          name: data
-        },
-        hashAlg: hashAlg,
-        processType: processType,
-        totalSigners: totalSigners
-      }, { headers: headers });
-  
-      if (response.status === 200) {
-        return response.data;
-      } else {
-        throw new Error(response.data.errorMessage);
-      }
     } catch (error) {
         console.error('Server response:', error);
-        throw new Error(error.response.data.errorMessage || 'Error');
+        throw new Error(error.response.data.description || 'Error desconocido durante la autenticación.');
     }
-  }
-  
-  
+}
+
+/**
+ * Convierte un archivo a formato Base64.
+ * @param {File} file - Archivo a convertir.
+ * @returns {Promise<string>} Datos del archivo en formato Base64.
+ * @throws {Error} Si hay un error durante la conversión.
+ */
+export function convertFileToBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result.split(',')[1]);
+        reader.onerror = error => reject(error);
+    });
+}
+
+/**
+ * Inicia un proceso multilateral de firma.
+ * @param {string} token - Token de autenticación.
+ * @param {string} data - Nombre del documento a firmar.
+ * @param {string} document2Sign - Documento en formato Base64.
+ * @param {string} hashAlg - Algoritmo de digestión (ej. SHA256).
+ * @param {string} processType - Tipo de proceso de firma (ej. PDF).
+ * @param {number} totalSigners - Número total de firmantes.
+ * @returns {Object} Respuesta del servidor.
+ * @throws {Error} Si hay un error en la solicitud.
+ */
+export async function startMultilateralProcess(token, data, document2Sign, hashAlg, processType, totalSigners) {
+    const headers = {
+        'Authorization': token,
+    };
+
+    try {
+        const response = await axios.post(`${BASE_URL}/multilateral/`, {
+            data: data,
+            document2Sign: {
+                base64: true,
+                data: document2Sign,
+                name: data
+            },
+            hashAlg: hashAlg,
+            processType: processType,
+            totalSigners: totalSigners
+        }, { headers: headers });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            throw new Error(response.data.errorMessage || 'Error desconocido al iniciar el proceso multilateral.');
+        }
+    } catch (error) {
+        console.error('Server response:', error);
+        throw new Error(error.response.data.errorMessage || 'Error desconocido al iniciar el proceso multilateral.');
+    }
+}
+
+/**
+ * Lee un archivo como texto.
+ * @param {File} file - Archivo a leer.
+ * @returns {Promise<string>} Contenido del archivo como texto.
+ * @throws {Error} Si hay un error durante la lectura.
+ */
+export function readFileAsText(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+/**
+ * Limpia el contenido del certificado, eliminando encabezados y pies de página.
+ * @param {string} content - Contenido del certificado.
+ * @returns {string} Certificado limpio.
+ */
+export function cleanCertificateContent(content) {
+    return content.replace(/-----BEGIN CERTIFICATE-----/g, '').replace(/-----END CERTIFICATE-----/g, '').trim();
+}
+
+/**
+ * Verifica si una cadena está en formato Base64.
+ * @param {string} str - Cadena a verificar.
+ * @returns {boolean} Verdadero si la cadena está en Base64, falso en caso contrario.
+ */
+function isBase64(str) {
+    try {
+        return btoa(atob(str)) === str;
+    } catch (err) {
+        return false;
+    }
+}
+
+/**
+ * Asegura que el contenido proporcionado esté en formato Base64.
+ * @param {string|Blob} input - Contenido o archivo a verificar.
+ * @returns {Promise<string>} Contenido en formato Base64.
+ * @throws {Error} Si el tipo de entrada no es válido.
+ */
+export async function ensureBase64Content(input) {
+    if (typeof input === 'string') {
+        return input;
+    } else if (input instanceof Blob) {
+        return await convertFileToBase64(input);
+    } else {
+        throw new Error("Invalid input type for ensureBase64Content");
+    }
+}
+
+/**
+ * Obtiene el hash FEA.
+ * @param {string} token - Token de autenticación.
+ * @param {number} multilateralId - ID del proceso multilateral.
+ * @param {string} certificate - Certificado en formato Base64.
+ * @returns {Object} Respuesta del servidor.
+ * @throws {Error} Si hay un error en la solicitud.
+ */
+export async function getHashFEA(token, multilateralId, certificate) {
+    const headers = {
+        'Authorization': token,
+    };
+
+    const body = {
+        signerCertificate: {
+            base64: true,
+            data: certificate,
+            evidenceType: "CERTIFICATE"
+        }
+    };
+
+    try {
+        const response = await axios.post(`${BASE_URL}/multilateral/getHash/${multilateralId}`, body, { headers: headers });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            throw new Error(response.data.errorMessage || 'Error desconocido al obtener el hash FEA.');
+        }
+    } catch (error) {
+        console.error('Server response:', error);
+        throw new Error(error.response.data.errorMessage || 'Error desconocido al obtener el hash FEA.');
+    }
+}
--- src/AuthenticationForm.js
+++ src/AuthenticationForm.js
@@ -1,49 +1,65 @@
 import React, { useState } from 'react';
-import { authenticate, startMultilateralProcess, convertFileToBase64 } from './Api';
+import { authenticate, startMultilateralProcess, convertFileToBase64, getHashFEA, readFileAsText, cleanCertificateContent, ensureBase64Content } from './Api';
 
-
+/**
+ * Formulario de autenticación y envío de documentos para firma.
+ */
 function AuthenticationForm() {
-  const [user, setUser] = useState('');
-  const [password, setPassword] = useState('');
-  const [file, setFile] = useState(null);
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [file, setFile] = useState(null);
+    const [certificate, setCertificate] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    try {
-      const token = await authenticate(user, password);
-        console.log('Token:', token);
-      // Convertir el archivo a Base64
-      const base64Data = await convertFileToBase64(file);
+    /**
+     * Maneja el envío del formulario.
+     * @param {Event} e - Evento de envío del formulario.
+     */
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-      const response = await startMultilateralProcess(token, file.name, base64Data, 'SHA256', 'PDF', 1);
-      console.log('Response:', response);
-    } catch (error) {
-      console.error('Error:', error);
+        try {
+            const token = await authenticate(user, password);
+            console.log('Token:', token);
+
+            const base64Data = await convertFileToBase64(file);
+
+            const cleanedCertificateContent = cleanCertificateContent(await readFileAsText(certificate));
+            const finalCertificateContent = await ensureBase64Content(cleanedCertificateContent);
+
+            const response = await startMultilateralProcess(token, file.name, base64Data, 'SHA256', 'PDF', 1);
+            const getHashFea = await getHashFEA(token, response.multilateralId, finalCertificateContent);
+
+            console.log('Hash FEA:', getHashFea);
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
-}
-  
 
-  return (
-    <div>
-      <h2>Autenticación SeguriData</h2>
-      <form onSubmit={handleSubmit}>
+    return (
         <div>
-          <label>Usuario:</label>
-          <input type="text" value={user} onChange={(e) => setUser(e.target.value)} required />
+            <h2>Prueba de concepto SeguriData</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Usuario:</label>
+                    <input type="text" value={user} onChange={(e) => setUser(e.target.value)} required />
+                </div>
+                <div>
+                    <label>Contraseña:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <div>
+                    <label>Documento a firmar:</label>
+                    <input type="file" onChange={(e) => setFile(e.target.files[0])} required />
+                </div>
+                <div>
+                    <label>Certificado:</label>
+                    <input type="file" onChange={(e) => setCertificate(e.target.files[0])} required />
+                </div>
+                <button type="submit">Enviar</button>
+            </form>
         </div>
-        <div>
-          <label>Contraseña:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <div>
-          <label>Documento a firmar:</label>
-          <input type="file" onChange={(e) => setFile(e.target.files[0])} required />
-        </div>
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default AuthenticationForm;
